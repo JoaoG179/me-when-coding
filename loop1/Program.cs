d03b035fe3945bd980cd2732be78f963eb70c0be
João Gabriel <joaogabrielffaria179@gmail.com>
2024-03-27 20:34:06 -0300

    please work

---
+++ loop1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Faça um programa que peça uma nota, entre zero e dez. 
+Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.*/
+
+using System;
+
+class score{
+    static void Main(string[] args){
+        int score;
+        for (;;)
+        {
+            Console.Write("Qual nota foi sua nota no teste?");
+            if (int.TryParse(Console.ReadLine(), out score))
+            {
+                if (score >=0 && score <=10)
+                {
+                    Console.WriteLine("sua nota no teste foi de {0} pontos.", score);
+                    break;
+                }else
+                {
+                    Console.WriteLine("essa não é uma nota valida, tente novamente");
+                }
+            }else
+            {
+                Console.WriteLine("essa não é uma nota valida, tente novamente");
+            }
+        }
+    }
+}
+
---
+++ loop2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário
+mostrando uma mensagem de erro e voltando a pedir as informações.*/
+
+using System;
+
+class name{
+    static void Main(string[] args){
+        string name, password;
+        Console.Write("insira o nome de usuario: ");
+        name=Console.ReadLine();
+        for(;;){
+            Console.Write("insira a senha: ");
+            password=Console.ReadLine();
+            if (password != name){
+                Console.WriteLine("Processo completo.");
+                break;
+            }else{
+                Console.WriteLine("Sua senha não pode ser igual ao seu nome de usuário. Tente novamente");
+            }
+        }
+    }
+}
